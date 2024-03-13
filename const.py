4e1f2e9b37c6c826b2f96ed8eb6588a68cbe52dc
XiaoColorful <XColor_ful@outlook.com>
2024-03-13 22:48:50 +0800

    常量

---
+++ const.py
@@ -0,0 +1,10 @@
+TYPE_DEFAULT_CONTAINER = "Default_Template"
+TYPE_EXTRA_CONTAINER = "Extra_Template"
+TYPE_DEFAULT_METHOD = "Default_Template"
+TYPE_EXTRA_METHOD = "Extra_Template"
+TYPE_DEFAULT_CLASS_FUNC = "default_container_func"
+TYPE_EXTRA_CLASS_FUNC = "extra_container_func"
+CONDITION_SUCCESS = True # 正常运行
+CONDITION_FAIL = False # 程序报错
+BLOCK_LIST = ["|||", "\t"]
+EXIT = "exit"
