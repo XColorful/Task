0c149cdbb21c6c06fecc27ff8ae210194941856d
XiaoColorful <XColor_ful@outlook.com>
2024-03-16 01:09:39 +0800

    添加settings_dict默认值

---
+++ val.py
@@ -0,0 +1,16 @@
+# 初始化变量
+main_container_list = []
+class_func_list = []
+df_container_template_list = []
+ex_container_template_list = []
+io_list = []
+message_list = []
+method_list = []
+df_task_template_list = []
+ex_task_template_list = []
+# 设置默认值
+settings_dict = {"SHOW_TIPS":True,
+                "AUTO_SAVE":True,
+                "AUTO_BACKUP":True,
+                "BACKUP_INTERVAL":3,
+                "BACKUP_TOTAL":3}
