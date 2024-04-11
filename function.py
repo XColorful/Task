013095959a2b79b9b0ad722ef2f2795d128436ec
XiaoColorful <XColor_ful@outlook.com>
2024-04-11 23:00:27 +0800

    fix清理文件逻辑
    修复pkl文件备份，清理的顺序

--- function.py
+++ function.py
@@ -145,19 +145,21 @@ def backup_pkl(data, dir:str, system_pkg:dict, interval : int = 3, backup_total 
     if pkl_file_list:
         file_basename = basename(pkl_file_list[-1])
         last_backup_time = datetime.strptime(file_basename[7:28], "%Y_%m_%d - %H-%M-%S")
-        # 如果没超过，直接返回
-        if (current_date - last_backup_time).days < date_interval: return None
-        else: display_last_backup_time = f"（上次备份：{last_backup_time.strftime("%Y_%m_%d - %H-%M-%S")}）"
-    # 写入备份pkl文件
-    backup_dir = join(dir, file_name)
-    with open(backup_dir, 'wb') as f: pickle.dump(data, f)
+        # 如果没超过
+        if (current_date - last_backup_time).days < date_interval: pass
+        else:
+            display_last_backup_time = f"（上次备份：{last_backup_time.strftime("%Y_%m_%d - %H-%M-%S")}）"
+            # 写入备份pkl文件
+            backup_dir = join(dir, file_name)
+            with open(backup_dir, 'wb') as f: pickle.dump(data, f)
+            system_pkg["system_msg"](f"自动备份pkl文件至\"{backup_dir}\"")
+            system_pkg["body_msg"]([f"文件大小：{file_size_str(backup_dir)}", f"备份间隔：{date_interval}天{display_last_backup_time}", f"备份总数：{backup_count}/{backup_total}"])
     # pkl数量超过backup_total，则删除最老的一个
     backup_count = len(pkl_file_list) + 1
     if backup_count > backup_total:
         remove(pkl_file_list[0])
         backup_count -= 1
-    system_pkg["system_msg"](f"自动备份pkl文件至\"{backup_dir}\"")
-    system_pkg["body_msg"]([f"文件大小：{file_size_str(backup_dir)}", f"备份间隔：{date_interval}天{display_last_backup_time}", f"备份总数：{backup_count}/{backup_total}"])
+        system_pkg["system_msg"]("已清理较老的pkl文件")
 
 def table_analyze_result(method_info_list:list, method_index_list:list, system_pkg:dict, start_index = 0):
     """[标签，版本，适用类型，指令集]
