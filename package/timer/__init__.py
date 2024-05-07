b68b95cf46bfe0ddad152214a04026793ffeae03
XiaoColorful <XColor_ful@outlook.com>
2024-05-08 01:29:54 +0800

    更正类名

--- package/timer/__init__.py
+++ package/timer/__init__.py
@@ -1,11 +1,11 @@
 from .class_func import timer_tasker_func
 from .tasker import timer_tasker
-from .method import method_template
+from .method import timer_task_method
 from .task import timer_task
 package_dict = {"class_func":[timer_tasker_func],
                 
                 "extra_tasker":[timer_tasker],
                 
-                "method":[method_template()],
+                "method":[timer_task_method()],
                 
                 "extra_task":[timer_task]}
