e748987a0ff80a92a1ac19780e4bc3701408c0ad
XiaoColorful <XColor_ful@outlook.com>
2024-07-15 15:51:39 +0800

    添加配置流程

--- package/timer/update_info.py
+++ package/timer/update_info.py
@@ -1,4 +1,4 @@
-from .function import YYYY_MM_DD
+from .function import YYYY_MM_DD, set_timer_task_df_attribute, set_timer_task_prefix, set_timer_task_df_content
 
 def timer_update_info(self, system_pkg) -> tuple:
     block_list = system_pkg["BLOCK_LIST"] + [" "]
@@ -22,6 +22,26 @@ def timer_update_info(self, system_pkg) -> tuple:
         if self.task_template == []: # 无可用task模板类型
             system_pkg["system_msg"](f"没有可用的task模板类型（{self.type}）")
             return (system_pkg["CONDITION_SUCCESS"], f"无可用task模板类型（{self.type}）")
+    
+    input_config = True
+    
+    if input_config == True:
+        if self.timer_task_df_attribute == "":
+            system_pkg["tips_msg"]("可在之后使用\"config\"命令更改")
+            if set_timer_task_df_attribute(self, system_pkg) == False:
+                input_config = False
+    if input_config == True:
+        if self.timer_task_prefix == "":
+            system_pkg["tips_msg"]("可在之后使用\"config\"命令更改")
+            if set_timer_task_prefix(self, system_pkg) == False:
+                input_config = False
+        
+    if input_config == True:
+        if self.timer_task_df_content == "":
+            system_pkg["tips_msg"]("可在之后使用\"config\"命令更改")
+            if set_timer_task_df_content(self, system_pkg) == False:
+                input_config = False
+                system_pkg["system_msg"]("这是最后一个config了。。之后可用\"config\"命令更改")
     
     if self.create_date == "": # 更新创建日期
         self.create_date = YYYY_MM_DD()
